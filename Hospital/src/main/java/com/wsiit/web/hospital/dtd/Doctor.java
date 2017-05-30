35947d9876d85722da5b980bc43258ec39048f27
aashish <aashish.bal@gmail.com>
2017-05-30 16:08:42 +0530

    addes setter and getter for Docotor

--- Hospital/src/main/java/com/wsiit/web/hospital/dtd/Doctor.java
+++ Hospital/src/main/java/com/wsiit/web/hospital/dtd/Doctor.java
@@ -3,6 +3,17 @@ package com.wsiit.web.hospital.dtd;
 public class Doctor {
 
 	private int deptNo;
+	
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
 
 	@Override
 	public String toString() {
